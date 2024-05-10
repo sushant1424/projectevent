ec27e4bdae90666e3e3830ccb505a200ac62a952
sushant1424 <sushantshrestha184@gmail.com>
2024-05-10 21:38:33 +0545

    make a index page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Upscale Events</title>
+</head>
+<body>
+  <!-- Header section -->
+  <?php
+  include("header.php");
+  ?>
+  
+
+  <!-- Main Content -->
+  
+</body>
+</html>
