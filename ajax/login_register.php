71dfcc26db5d9a9f9207a2ffec664035ee93b8de
Sushant.peace <141411471+sushant1424@users.noreply.github.com>
2024-06-21 07:20:00 +0545

    Add files via upload

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -1,87 +1,76 @@
 <?php
-require('../admin/inc/essentials.php');
 require('../admin/inc/db_config.php');
-require('../sendgrid/sendgrid-php.php');
-function sendMail($user_email,$name,$token)
-{
-  $email = new \SendGrid\Mail\Mail();
-  $email->setFrom("shresthasushant227@gmail.com", "Upscale");
-  $email->setSubject("Account verification");
-  $email->addTo("$user_email", "$name");
-  
-  $email->addContent(
-    "text/html",
-    "Click to confirm your email:<br>
-    <a href='".SITE_URL."email_confirm.php?email_confirmation&email=$user_email&token=$token"."'>
-    CLICK ME
-    </a>
-    "
-  );
-  $sendgrid = new \SendGrid(SENDGRID_API_KEY);
-  try {
+require('../admin/inc/essentials.php');
 
-     $sendgrid->send($email);
-      return 1;
+?>
+
+<?php
+
+if(isset($_POST['register'])) {
+    $data = filtration($_POST);
+    
+    if ($data['password'] != $data['confirm_pass']) {
+        echo 'pass_mismatch';
+        exit;
+    }
+
+    $user_exists = select("SELECT * FROM `user_register` WHERE `email` = ? AND `phone` =? LIMIT 1", [$data['email'], $data['phone']], "ss");
+
+    if (mysqli_num_rows($user_exists) != 0) {
+        $user_exists_fetch = mysqli_fetch_assoc($user_exists);
+        echo ($user_exists_fetch['email'] == $data['email']) ? 'email_already' : 'phone_already';
+        exit;
+    }
+
+    $token = bin2hex(random_bytes(16));
    
-  } catch (Exception $e) {
-   return 0;
-  }
+    $q = "INSERT INTO `user_register`( `username`, `email`, `password`, `phone`,  `address`,  `token`) VALUES (?,?,?,?,?,?)";
+    $values = [$data['username'], $data['email'], $data['password'], $data['phone'],  $data['address'],  $token];
+
+    if (insert($q, $values, "ssssss")) {
+         echo 1;
+    } else {
+        echo 'ins_failed';
+    }
 }
 
-if (isset($_POST['register'])) {
-  $data = filtration($_POST);
+if(isset($_POST['login'])){
+    $data = filtration($_POST);
 
-  //PASSWORD AND CONFIRM PASSWORD MATCH
+    $user_exists = select("SELECT * FROM `user_register` WHERE `email`=? OR `phone`=? LIMIT 1",[$data['email_mobile'],$data['email_mobile']],"ss");
 
-  if ($data['password'] != $data['confirm_pass']) {
-    echo 'pass_mismatch';
-    exit;
-  }
+    if(mysqli_num_rows($user_exists)==0){
+       echo 'inv_email_mobile';
+       exit; 
+    }
+    else{
+        $users_fetch = mysqli_fetch_assoc($user_exists);
+        if($users_fetch['is_verified']==0){
+            echo 'not_verified';
+            exit;
+        }
+        else if($users_fetch['status']==0){
+            echo 'inactive';
+            exit;
+        }
+        else{
+            if(password_verify($data['password'],$users_fetch['password'])){
+                echo "invalid_pass";
+            }
+            else{
+                session_start();
+                $_SESSION['login']= true;
+                $_SESSION['uid'] =  $users_fetch['id'];
+                $_SESSION['uname'] =  $users_fetch['username'];
+                $_SESSION['phone'] =  $users_fetch['phone'];
+                echo 1;
 
-  // exists or not
-  //select from table and then if match is found then stop executing query
-  $user_exists = select("SELECT * FROM `user_register` WHERE `email` = ? OR `phone` =? LIMIT 1", [$data['email'], $data['phone']], "ss");
+            }
+        }
+    }
 
-  if (mysqli_num_rows($user_exists) != 0) {
-    $user_exists_fetch = mysqli_fetch_assoc($user_exists);
-    echo ($user_exists_fetch['email'] == $data['email']) ? 'email_already' : 'phone_already';
-    exit;
-  }
-
-  //uploading user image to the server
-
-  $img = uploadUserPicture($_FILES['picture']);
-
-  if ($img == 'inv_img') {
-    echo "inv_img";
-    exit;
-  } else if ($img == 'upd_failed') {
-    echo "upd_failed";
-    exit;
-  }
-
-
-
-  // confirmation link to email
-
-  $token = bin2hex(random_bytes(16)); //generate random byte codes.
-
-  if(sendMail($data['email'],$data['username'],$token)){
-    echo "mail_failed";
-    exit;
-  }
-
-  $enc_password = password_hash($data['password'],PASSWORD_BCRYPT); //for encrypting the password
-
-  $sql = "INSERT INTO `user_register`( `username`, `email`, `password`, `phone`, `photo`, `address`, `dob`, `token`) VALUES (?,?,?,?,?,?,?,?,?)";
-
-  $values= [$data['username'],$data['email'],$enc_password,$data['phone'],$img['photo'],$data['address'],$data['dob'],$token];
-
-  if(insert($sql,$values,'sssssssss')){
-    echo  1;
-  }
-  else{
-    echo 'ins_failed';
-  }
 
 }
+
+?>
+
